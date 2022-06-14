ed823682d40010f5d432dba5d7d191031e12a6bd
unknown <bengera89@gmail.com>
2022-06-14 17:12:11 +0700

    desktop
    creations-page

--- js/main.js
+++ js/main.js
@@ -2,12 +2,15 @@ const menu = document.querySelector('.hamburger-menu');
 const nav = document.querySelector('.nav');
 const logo = document.querySelector('.header__logo');
 
+// const button = document.querySelector('.creations__btn');
+
 
 menu.addEventListener('click', () => {
     console.log('toggle-menu');
     menu.classList.toggle('active');
     nav.classList.toggle('active');
     logo.classList.toggle('active');
+    
     
     
 })
