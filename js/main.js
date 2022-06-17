708cfe3a597bd3ba8885cd32e5a8a908c99315bf
unknown <bengera89@gmail.com>
2022-06-17 14:32:43 +0700

    final

--- js/main.js
+++ js/main.js
@@ -4,17 +4,14 @@ const button = document.querySelector('.creations__btn');
 const buttonDesk = document.querySelector('.creations__btn--desktop');
 
 
+
 menu.addEventListener('click', () => {
     console.log('toggle-menu');
     menu.classList.toggle('active');
     nav.classList.toggle('active');
-    
-    
 
-
-    
+       
+      
        
 })
-
-
 
